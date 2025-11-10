5886391b3696aa599adfede7e4289295cb6e2337
DUCANHGHEL <aduc7246@gmail.com>
2025-11-10 12:57:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require("path");
+
+// Serve static frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+// Khi vào root "/", tự động trả về file index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }, // Neon thường cần SSL
